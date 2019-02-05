4862406d6dff3909699f8f43f243077f002cb5bb
Jefferson Cardoso <jefferson.mcardoso@gmail.com>
2019-02-04 23:12:05 -0200

    Configurando mapa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Environment } from '@ionic-native/google-maps/ngx';
 
 @Component({
   selector: 'app-root',
@@ -19,6 +20,15 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
+      
+      Environment.setEnv({
+        // api key for server
+        'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyA4keBrdSz15AJPLj1BC8U9t7ankc-kHb4',
+
+        // api key for local development
+        'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyA4keBrdSz15AJPLj1BC8U9t7ankc-kHb4'
+      });
+
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
